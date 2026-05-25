31337788e80e59e62b97aa3604ccdbcf933ca7a7
BikashkumarPalai <bikashkumarpalai04@gmail.com>
2026-05-25 11:46:51 +0530

    ram

--- routes/product.js
+++ routes/product.js
@@ -2,6 +2,7 @@ const express = require('express')
 const router = express.Router()
 const authMiddleware = require('../middleware/auth')
 const { addProduct, getMyProducts, updateProduct, deleteProduct, getProductsByShop, getPopularProducts } = require('../controllers/productController')
+const upload = require('../middleware/upload') 
 
 router.post('/add', authMiddleware, upload.single('image'), addProduct)
 router.get('/my-products', authMiddleware, getMyProducts)
--- routes/shop.js
+++ routes/shop.js
@@ -2,6 +2,7 @@ const express = require('express')
 const router = express.Router()
 const authMiddleware = require('../middleware/auth')
 const { createShop, getMyShop, updateShop, getAllShops, getShopById } = require('../controllers/shopController')
+const upload = require('../middleware/upload') 
 
 router.post('/create', authMiddleware, upload.single('image'), createShop)  // This fiels name 'image' should be match with the formdata which is dent by fronted
 router.get('/my-shop', authMiddleware, getMyShop)
